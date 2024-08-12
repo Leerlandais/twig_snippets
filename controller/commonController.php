e6dfee4a75a2bbc2985f7b44ee32bcc722dd307a
leerlandais <leerlandais@gmail.com>
2024-08-12 15:48:26 +0100

    publicView: added public homeview page and corrected alignment error in base.twig

--- controller/commonController.php
+++ controller/commonController.php
@@ -4,6 +4,6 @@ $route = $_GET['route'] ?? 'home';
 
 switch ($route) {
     case 'home':
-    echo $twig->render('template.html.twig');
+    echo $twig->render('publicView/public.home.html.twig');
         break;
 }
