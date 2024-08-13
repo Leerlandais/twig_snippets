8fcec89214a1f75382f9a0b06cf8ef791bf884aa
leerlandais <leerlandais@gmail.com>
2024-08-13 15:10:57 +0100

    FormManager: addNewForm function : OK

--- controller/privateController.php
+++ controller/privateController.php
@@ -1,6 +1,35 @@
 <?php
 use model\Manager\UserManager;
+use model\Manager\FormManager;
+use model\Mapping\FormMapping;
 
+// $formMapping = new FormMapping($db);
+$formManager = new FormManager($db);
+
+// ADD NEW FORM
+
+if (isset(
+    $_POST["addFormClass"],
+    $_POST["addFormType"],
+    $_POST["addFormTitle"],
+    $_POST["addFormDesc"],
+    $_POST["addFormImage"],
+    $_POST["addFormCode"]
+)) {
+    $formMappingData = [
+        'snip_form_class' => $_POST["addFormClass"],
+        'snip_form_type' => $_POST["addFormType"],
+        'snip_form_title' => $_POST["addFormTitle"],
+        'snip_form_desc' => $_POST["addFormDesc"],
+        'snip_form_img' => $_POST["addFormImage"],
+        'snip_form_code' => $_POST["addFormCode"]
+    ];
+
+    $formMapping = new FormMapping($formMappingData);
+
+    $formManager = new FormManager($db);
+    $formManager->addNewForm($formMapping);
+}
 
 
 
--- model/Manager/FormManager.php
+++ model/Manager/FormManager.php
@@ -4,14 +4,31 @@ namespace model\Manager;
 
 use model\Abstract\AbstractManager;
 use model\Mapping\FormMapping;
-use model\MyPDO;
-use MongoDB\Driver\Manager;
+
+
 
 class FormManager extends AbstractManager
 {
 
-    public function __construct(MyPDO $db) {
-        $this->db = $db;
+    public function addNewForm(FormMapping $formMapping): void {
+        $class = $formMapping->getSnipFormClass();
+        $type = $formMapping->getSnipFormType();
+        $title = $formMapping->getSnipFormTitle();
+        $desc = $formMapping->getSnipFormDesc();
+        $image = $formMapping->getSnipFormImg();
+        $code = $formMapping->getSnipFormCode();
+
+
+        $query = $this->db->prepare("INSERT INTO `snippets_forms`
+                                    (`snip_forms_class`, 
+                                    `snip_form_type`, 
+                                    `snip_form_title`, 
+                                    `snip_form_desc`, 
+                                    `snip_form_img`, 
+                                    `snip_form_code`) VALUES (?, ?, ?, ?, ?, ?)");
+
+        $query->execute([$class, $type, $title, $desc, $image, $code]);
+
     }
 
 }
